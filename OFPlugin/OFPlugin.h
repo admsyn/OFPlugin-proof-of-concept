80e88ea1981e6dd78aaadee2567e5d918106ab60
Adam Carlucci <carlucci.adam@gmail.com>
2013-08-14 12:18:34 +0900

    Initial Commit

---
+++ OFPlugin/OFPlugin.h
@@ -0,0 +1,13 @@
+//
+//  OFPlugin.h
+//  OFPlugin
+//
+//  Created by Adam on 2013-08-14.
+//  Copyright (c) 2013 admsyn. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface OFPlugin : NSObject
+
+@end
